41771e51fd1259d5a5443857c1b4bc8dcbdbb14d
likun <likun@DESKTOP-UHDSHD3>
2025-03-07 19:10:26 +0800

    添加项目文件。

---
+++ CalcAndFilter/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CalcAndFilter.Model;
+
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalcAndFilter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ViewModel viewModel { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = new ViewModel();
+            this.DataContext = viewModel;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            viewModel.ClacModel1.CalcContent = System.Text.Json.JsonSerializer.Serialize(viewModel.ClacModel1);
+        }
+    }
+}
---
+++ CalcAndFilter/Model/ViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcAndFilter.Model
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ClacModel ClacModel1 { get; set; }
+        public ClacModel ClacModel2 { get; set; }
+        public ClacModel ClacModel3 { get; set; }
+        private string filter;
+        public string Filter
+        {
+            get { return filter; }
+            set
+            {
+                filter = value;
+                OnPropertyChanged(nameof(Filter));
+            }
+        }
+
+        public ViewModel()
+        {
+            ClacModel1 = new ClacModel();
+            ClacModel2 = new ClacModel();
+            ClacModel3 = new ClacModel();
+            Filter = "";
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class ClacModel : INotifyPropertyChanged
+    {
+        public ClacModel()
+        {
+            CalcContent = "";
+            CalcResult = "";
+            IsFormula = false;
+            IsBitwise = false;
+            IsContinuous = false;
+        }
+        private string calcContent;
+        public string CalcContent
+        {
+            get { return calcContent; }
+            set
+            {
+                calcContent = value;
+                OnPropertyChanged(nameof(CalcContent));
+            }
+        }
+        private string calcResult;
+        public string CalcResult
+        {
+            get { return calcResult; }
+            set
+            {
+                calcResult = value;
+                OnPropertyChanged(nameof(CalcResult));
+            }
+        }
+        private bool isFormula;
+        public bool IsFormula
+        {
+            get { return isFormula; }
+            set
+            {
+                isFormula = value;
+                OnPropertyChanged(nameof(IsFormula));
+            }
+        }
+        private bool isBitwise;
+        public bool IsBitwise
+        {
+            get { return isBitwise; }
+            set
+            {
+                isBitwise = value;
+                OnPropertyChanged(nameof(IsBitwise));
+            }
+        }
+        private bool isContinuous;
+        public bool IsContinuous
+        {
+            get { return isContinuous; }
+            set
+            {
+                isContinuous = value;
+                OnPropertyChanged(nameof(IsContinuous));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
